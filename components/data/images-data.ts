73c4c9f4d7e9a6de3a746ed11465fc1f0a27d4c3
Yuri-NagaSaki <sayyikue@gmail.com>
2025-04-27 17:06:52 +0800

    fix

--- components/data/images-data.ts
+++ components/data/images-data.ts
@@ -1,32 +1,32 @@
 import { type ImageItem } from '../carousel-section';
 
 export const imagesData: ImageItem[] = [
-  { 
-    src: '/img/image1.png', 
+  {
+    src: '/img/image1.webp',
     alt: 'ImageFlow Screenshot 1',
     width: 1920,
     height: 1080
   },
-  { 
-    src: '/img/image2.png', 
+  {
+    src: '/img/image2.webp',
     alt: 'ImageFlow Screenshot 2',
     width: 1920,
     height: 1080
   },
-  { 
-    src: '/img/image3.png', 
+  {
+    src: '/img/image3.webp',
     alt: 'ImageFlow Screenshot 3',
     width: 1920,
     height: 1080
   },
-  { 
-    src: '/img/image4.png', 
+  {
+    src: '/img/image4.webp',
     alt: 'ImageFlow Screenshot 4',
     width: 1920,
     height: 1080
   },
-  { 
-    src: '/img/image5.png', 
+  {
+    src: '/img/image5.webp',
     alt: 'ImageFlow Screenshot 5',
     width: 1920,
     height: 1080
